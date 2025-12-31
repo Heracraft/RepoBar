d49d264d951a3f20ca81b06adf21dfccdd12c57e
Peter Steinberger <steipete@gmail.com>
2025-12-31 19:32:47 +0100

    style: widen menu padding

--- Sources/RepoBar/Support/MenuStyle.swift
+++ Sources/RepoBar/Support/MenuStyle.swift
@@ -3,23 +3,23 @@ import SwiftUI
 enum MenuStyle {
     static let menuItemContainerTrailingPadding: CGFloat = 10
 
-    static let cardHorizontalPadding: CGFloat = 10
+    static let cardHorizontalPadding: CGFloat = 20
     static let cardVerticalPadding: CGFloat = 6
     static let cardSpacing: CGFloat = 6
-    static let cardSeparatorInset: CGFloat = 10
+    static let cardSeparatorInset: CGFloat = 20
     static let cardSeparatorVerticalPadding: CGFloat = 4
 
-    static let filterHorizontalPadding: CGFloat = 12
+    static let filterHorizontalPadding: CGFloat = 24
     static let filterVerticalPadding: CGFloat = 3
 
-    static let headerHorizontalPadding: CGFloat = 10
+    static let headerHorizontalPadding: CGFloat = 20
     static let headerTopPadding: CGFloat = 10
     static let headerBottomPadding: CGFloat = 6
 
-    static let sectionHorizontalPadding: CGFloat = 12
+    static let sectionHorizontalPadding: CGFloat = 24
     static let sectionVerticalPadding: CGFloat = 8
 
-    static let bannerHorizontalPadding: CGFloat = 10
+    static let bannerHorizontalPadding: CGFloat = 20
     static let bannerVerticalPadding: CGFloat = 4
 
     static let submenuIconColumnWidth: CGFloat = 18
